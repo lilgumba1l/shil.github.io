e5567307b3ef6ecd2451eef5be93701832ae7237
lilgumba1l <74672884+lilgumba1l@users.noreply.github.com>
2024-10-02 18:26:58 +0300

    Add files via upload

--- js/script_main.js
+++ js/script_main.js
@@ -1,22 +1,29 @@
 // Открытие модального окна с информацией
 document.querySelectorAll('.product-card').forEach(function(card) {
-    card.addEventListener('click', function() {
-        const productName = card.getAttribute('data-name');
-        const productId = card.getAttribute('data-id');
-        const productPrice = card.getAttribute('data-price');
-        const productImage = card.getAttribute('data-image');
-        const productDescription = card.getAttribute('data-description');
+    card.addEventListener('click', function(event) {
+        // Проверяем, был ли клик на кнопке "Добавить в корзину" или на элементе с классом "product__quantity"
+        const isBuyButton = event.target.closest('.buy-button');
+        const isQuantityButton = event.target.closest('.product__quantity');
         
-        // Заполняем модальное окно данными
-        document.getElementById('modalProductName').innerText = productName;
-        document.getElementById('modalProductDescription').innerText = productDescription;
-        document.getElementById('modalProductId').innerText = `ID: ${productId}`;
-        document.getElementById('modalProductPrice').innerText = `Цена: ${productPrice} ₴`;
-        document.getElementById('modalProductImage').src = productImage;
+        // Если клик был на кнопке "Добавить в корзину" или на элементе "product__quantity", не открываем модальное окно
+        if (!isBuyButton && !isQuantityButton) {
+            const productName = card.getAttribute('data-name');
+            const productId = card.getAttribute('data-id');
+            const productPrice = card.getAttribute('data-price');
+            const productImage = card.getAttribute('data-image');
+            const productDescription = card.getAttribute('data-description');
+            
+            // Заполняем модальное окно данными
+            document.getElementById('modalProductName').innerText = productName;
+            document.getElementById('modalProductDescription').innerText = productDescription;
+            document.getElementById('modalProductId').innerText = `ID: ${productId}`;
+            document.getElementById('modalProductPrice').innerText = `Цена: ${productPrice} ₴`;
+            document.getElementById('modalProductImage').src = productImage;
 
-        // Показываем модальное окно
-        const modal = document.getElementById('productModal');
-        modal.classList.add('show');
+            // Показываем модальное окно
+            const modal = document.getElementById('productModal');
+            modal.classList.add('show');
+        }
     });
 });
 
